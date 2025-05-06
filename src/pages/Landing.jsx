605806cadb5802200bb0ace2c451243652b0cb3c
gauri jindal <gaurijindal@gauri-jindals-MacBook-Pro.local>
2025-05-07 00:30:11 +0530

    landing page html done

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+import "./Landing.css";
+const Landing = () => {
+  return (
+    <div className="landing">
+        <div className="landing-card">
+        <img
+          src="src/assets/landing-illustration.svg"
+          alt="Quiz Illustration"
+          className="landing-img"
+        />
+        <h1>
+            Welcome to Quizy!🧩
+        </h1>
+<p className="landing-buttons">
+    <Link to="/quiz" className="btn btn-primary">
+    Start Quiz
+    </Link>
+    <Link to="/about" className='btn btn-secondary'>
+    Learn More
+    </Link>
+</p>
+        </div>
+    </div>
+  )
+}
+
+export default Landing
