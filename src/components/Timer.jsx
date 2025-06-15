8cfe2638c218d196de81d4c7f015d18b2e02cc5c
Jgauri24 <jindalgauri244@gmail.com>
2025-06-15 23:06:54 +0530

    timer component with styling is completed

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 
-const Timer = () => {
+const Timer = ({timeLimit,onTimeUp}) => {
+  const [timeLeft,setTimeLeft]=useState()
+  useEffect(()=>{
+    if (timeLeft<=0){
+      onTimeUp()
+    }
+    const timer=setInterval(()=>{
+      setTimeLeft(el=>el-1)
+    },1000)
+    return ()=>clearInterval(timer)
+  },[timeLeft,onTimeUp])
   return (
-    <div>Timer</div>
+<div className="timer">
+<div className="timer-circle">
+  <span className="timer-text">{timeLeft}s</span>
+</div>
+</div>
+
   )
 }
 
