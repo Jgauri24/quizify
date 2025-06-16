911f9ec2510d042d93a9a0d3b4c67fe06f28f7d2
Jgauri24 <jindalgauri244@gmail.com>
2025-06-16 14:18:16 +0530

    questioncard component with styling is created

--- src/components/QuestionCard.jsx
+++ src/components/QuestionCard.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
-
-const QuestionCard = () => {
+import React from "react";
+import "./QuestionCard.css";
+const QuestionCard = ({
+  question,
+  options,
+  selectedAnswer,
+  onSelect,
+  isDisabled,
+}) => {
   return (
-    <div>QuestionCard</div>
-  )
-}
+    <div className="question-card">
+      <h2 className="question-text">{question}</h2>
+      <div className="options-container">
+        {options.map((el, i) => (
+          <button
+            className={`option-btn ${selectedAnswer === el ? "selected" : ""}`}
+            key={i}
+            onClick={() => onSelect(el)}
+            disabled={isDisabled}
+          >
+            {el}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default QuestionCard
+export default QuestionCard;
