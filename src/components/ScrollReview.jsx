71a64d8f866f8138e1aed3f2220ca5109207aa49
Jgauri24 <jindalgauri244@gmail.com>
2025-07-17 23:03:08 +0530

    done with the result page

--- src/components/ScrollReview.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ScrollReview = () => {
-  return (
-    <div>ScrollReview</div>
-  )
-}
-
-export default ScrollReview
--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
-
+import { useLocation, useNavigate } from 'react-router-dom';
+import ResultSummary from '../components/ResultSummary';
+import './Result.css';
 const Result = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const { score, totalQuestions, userAnswers, questions } = location.state;
+
+  const correctAnswers=questions.filter((q,i)=>
+    q.correctAnswer === userAnswers[i]).length
+  const handleReviewClick = () => {
+    navigate('/review', {
+        state: {
+            questions,
+            userAnswers
+        }
+    });
+};
   return (
-    <div>Result</div>
+<div className="result-container">
+  <div className="result-content">
+    <ResultSummary 
+    score={score}
+    correctAnswers={correctAnswers}
+    onReviewClick={handleReviewClick}
+    totalQuestions={totalQuestions}/>
+  </div>
+
+</div>
   )
 }
 
